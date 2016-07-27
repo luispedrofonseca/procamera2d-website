febb3e44033e9b1b926f4dd9feab989307f1f7f5
Luis Fonseca <luispedrofonseca@gmail.com>
2016-07-27 23:15:29 +0800

    WIP

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 	$('img[alt="YOUTUBE"]').each(function () 
 	{
 		var id = $(this).attr('src').split('/')[$(this).attr('src').split('/').length - 1];
-		var video = '<iframe style="width: 100%;height: 450px;" src="https://www.youtube.com/embed/' + id + '?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>';
+		var video = '<iframe class="youtube-player" src="https://www.youtube.com/embed/' + id + '?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>';
 		$(this).replaceWith(video);
 	});
 });
@@ -14,7 +14,7 @@ $(document).ready(function () {
 	$('img[alt="HEADER"]').each(function () 
 	{
 		var id = $(this).attr('src').split('/')[$(this).attr('src').split('/').length - 1];
-		var video = '<p></p><img src="/images/icons/' + id + '.png" width="83" style="float:left;padding-right:10px">';
+		var video = '<p></p><span class="user-guide-header-icon"><img src="/images/icons/' + id + '.png" width="83"></span>';
 		$(this).replaceWith(video);
 	});
 });
